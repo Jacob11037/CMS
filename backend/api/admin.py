e49780ed0e022b43f8bd571e4c6868ec231c3aa5
Priya Sajan <priyasajan1414@gmail.com>
2025-04-29 21:43:48 +0530

    backend

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,8 +4,8 @@ from django import forms
 from django.contrib.auth.models import User
 from rest_framework.exceptions import ValidationError
 
-from .models import Receptionist, Doctor, Patient, Department, Appointment, MedicalHistory, Medicine, LabTest, \
-    Prescription
+from .models import Receptionist, Doctor, Patient, Department, Appointment, MedicalHistory, Medicine, LabTest,Prescription,Pharmacist
+
 
 # # Admin form for creating a new user (admin will enter username and password)
 # class ReceptionistAdminForm(forms.ModelForm):
@@ -117,6 +117,7 @@ admin.site.register(MedicalHistory)
 admin.site.register(Medicine)
 admin.site.register(LabTest)
 admin.site.register(Prescription)
+admin.site.register(Pharmacist)
 
 
 
