2f5309c80549a34774bf6ac5724874e0c4468bde
Jacob <jacob_se@outlook.com>
2025-05-08 01:28:42 +0530

    removing localhost

--- frontend/src/app/pages/login/page.js
+++ frontend/src/app/pages/login/page.js
@@ -8,7 +8,7 @@ import '../../styles/loginpage.css';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL ;
 
 export default function LoginPage() {
   const [formData, setFormData] = useState({
